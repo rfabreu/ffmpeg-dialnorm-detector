db10f7ef8a00b9abea5eb0a13691bc5ff58365a7
Rafael Gomes <rafaelg@Rafaels-Laptop.local>
2025-07-26 13:43:31 -0400

    add streams.js to list stream definitions

---
+++ functions/streams.js
@@ -0,0 +1,15 @@
+const { createClient } = require("@supabase/supabase-js");
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+exports.handler = async () => {
+  try {
+    const { data, error } = await supabase.from("streams").select("*");
+    if (error) throw error;
+    return { statusCode: 200, body: JSON.stringify(data) };
+  } catch (err) {
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+  }
+};
